622ebf9d0c4409b671d3a0b3d0d744e0d40cf8fb
侯恩星 <houenxing@houenxingdeMacBook-Pro.local>
2018-08-16 19:29:58 +0800

    update

---
+++ src/components/home.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="home">
+    <p>Home</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+    }
+  },
+  methods: {
+    onQuery: function () {
+      this.$http.post('/boss/mysql', {
+        command: "login",
+        quote: "",
+      }).then(res => {
+        // post succeed！
+        console.log("Post succeed!")
+        console.log(res.data);
+      }, res => {
+        console.log("Post failed!")
+      });
+      this.$http.post('/boss/mysql', {
+        command: "query",
+        quote: "use test",
+      }).then(res => {
+        // post succeed！
+        console.log("Post succeed!")
+        console.log(res.data);
+      }, res => {
+        console.log("Post failed!")
+      });
+    }
+  },
+  created: function () {
+    this.onQuery();
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
